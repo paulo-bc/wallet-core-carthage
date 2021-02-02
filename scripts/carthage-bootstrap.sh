7a2fa6abf3e3818fb7cddefd69639eb2bf94fb42
Paulo <paulo@blockchain.com>
2021-02-02 20:26:37 +0000

    Carthage scripts for xcode 12

---
+++ scripts/carthage-bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+#  scripts/carthage-bootstrap.sh
+#
+#  What It Does
+#  ------------
+#  Workaround: Carthage builds fails at xcrun lipo on Xcode 12
+#  https://github.com/Carthage/Carthage/issues/3019
+#  https://github.com/Carthage/Carthage/blob/master/Documentation/Xcode12Workaround.md
+
+sh scripts/carthage.sh bootstrap --use-ssh --cache-builds --platform iOS
